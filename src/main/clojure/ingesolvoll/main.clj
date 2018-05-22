54751700e8c1d1c48978e4ca958be8a69d0210f3
ingesolvoll <inge.solvoll@gmail.com>
2018-05-22 15:27:24 +0200

    For now we don't support using this for clean

--- src/main/clojure/ingesolvoll/main.clj
+++ src/main/clojure/ingesolvoll/main.clj
@@ -1,6 +1,10 @@
 (ns ingesolvoll.main
-  (:require [leiningen.core.main :as lein]))
+  (:require [leiningen.core.main :as lein])
+  (:import (org.apache.maven.plugin MojoExecutionException)))
 
 (defn main [& args]
+
+      (when (-> args first (= "clean"))
+            (throw (MojoExecutionException. "This plugin cannot be used for cleaning project. Configure maven to do that.")))
       (with-redefs [lein/exit (constantly nil)]
                    (apply lein/-main args)))
